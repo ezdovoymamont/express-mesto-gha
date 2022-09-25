699cc3342fe0ff634db940106a0a0db9261e5a00
Mamont <muhina.katerina@yandex.ru>
2022-09-25 11:16:31 +0300

    fix

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,69 +1,69 @@
-const Card = require("../models/cardSchema");
+const Card = require('../models/cardSchema');
 
 module.exports.getCards = (req, res) => {
   Card.find()
-    .then(cards => res.send(cards));
+    .then((cards) => res.send(cards));
 };
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
-  if(name === undefined) {
+  if (name === undefined) {
     res.status(400).send({ message: 'Поле name должно быть заполнено' })
     return;
   }
-  if(link === undefined) {
+  if (link === undefined) {
     res.status(400).send({ message: 'Поле link должно быть заполнено' })
     return;
   }
-  const owner = req.user._id;
-  Card.create({name, link, owner})
-    .then(card => res.send({ data: card }))
-    .catch(err => res.status(500).send({ message: 'Произошла ошибка'}));
+  const owner = req.user.id;
+  Card.create({ name, link, owner })
+    .then((card) => res.send({ data: card }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка'}));
 };
 
 module.exports.deleteCard = (req, res) => {
-  Card.findByIdAndRemove(req._id)
-    .then(card => {
-      if(card === null){
+  Card.findByIdAndRemove(req.id)
+    .then((card) => {
+      if (card === null) {
         res.status(404).send({ message: 'Карточка не найдена' });
       }
-      else{
+      else {
         res.send({ data: card });
       }
     })
-    .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
 
 module.exports.addLike = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
-    { $addToSet: { likes: req.user._id } },
+    { $addToSet: { likes: req.user.id } },
     { new: true },
   )
-  .then(card => {
-    if(card === null){
-      res.status(404).send({ message: 'Карточка не найдена' });
-    }
-    else{
-      res.send({ data: card });
-    }
-  })
-  .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
-}
+    .then((card) => {
+      if (card === null) {
+        res.status(404).send({ message: 'Карточка не найдена' });
+      }
+      else {
+        res.send({ data: card });
+      }
+    })
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
 
 module.exports.removeLike = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
-    { $pull: { likes: req.user._id } },
+    { $pull: { likes: req.user.id } },
     { new: true },
   )
-  .then(card => {
-    if(card === null){
-      res.status(404).send({ message: 'Карточка не найдена' });
-    }
-    else{
-      res.send({ data: card });
-    }
-  })
-  .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
-}
+    .then((card) => {
+      if (card === null) {
+        res.status(404).send({ message: 'Карточка не найдена' });
+      }
+      else {
+        res.send({ data: card });
+      }
+    })
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
