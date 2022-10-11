72f29918e6d96a82554e529a4618547903d889fd
Mamont <>
2022-10-11 11:11:59 +0300

    fix

--- app.js
+++ app.js
@@ -76,4 +76,4 @@ app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
 
-module.exports = app;
+module.exports = linkPattern;
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,50 @@
 const express = require('express');
 const {
+  celebrate,
+  Joi,
+} = require('celebrate');
+const {
   getUsers, getUser, updateUser, updateAvatar,
 } = require('../controllers/users');
+const linkPattern = require('../app');
 
 const router = express.Router();
 
 router.get('/', getUsers);
 router.get('/me', getUser);
-router.get('/:id', getUser);
-router.patch('/me', updateUser);
-router.patch('/me/avatar', updateAvatar);
+router.get(
+  '/:id',
+  celebrate({
+    params: Joi.object()
+      .keys({
+        id: Joi.string()
+          .required()
+          .length(24),
+      }),
+  }),
+  getUser,
+);
+router.patch(
+  '/me',
+  celebrate({
+    body: Joi.object()
+      .keys({
+        name: Joi.string()
+          .required()
+          .min(2)
+          .max(30),
+        about: Joi.string()
+          .required()
+          .min(2)
+          .max(30),
+      }),
+  }),
+  updateUser,
+);
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().pattern(linkPattern).min(2).max(30),
+  }),
+}), updateAvatar);
 
 module.exports = router;
