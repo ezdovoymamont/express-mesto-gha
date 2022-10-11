ead8dee8ea2a202c5f992f40cfdde980f8514fd6
Mamont <>
2022-10-11 11:48:56 +0300

    joi

--- routes/cards.js
+++ routes/cards.js
@@ -6,6 +6,8 @@ const {
 const {
   getCards, createCard, deleteCard, addLike, removeLike,
 } = require('../controllers/cards');
+// eslint-disable-next-line prefer-regex-literals
+const linkPattern = new RegExp('^(?:http(s)?:\\/\\/)?[\\w.-]+(?:\\.[\\w\\.-]+)+[\\w\\-\\._~:/?#[\\]@!\\$&\'\\(\\)\\*\\+,;=.]+$');
 
 const router = express.Router();
 
@@ -14,10 +16,24 @@ router.delete('/:id', deleteCard);
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().min(1),
+    link: Joi.string().pattern(linkPattern).required().min(1),
   }),
 }), createCard);
-router.put('/:id/likes', addLike);
-router.delete('/:id/likes', removeLike);
+router.put('/:id/likes', celebrate({
+  params: Joi.object()
+    .keys({
+      id: Joi.string()
+        .required()
+        .length(24),
+    }),
+}), addLike);
+router.delete('/:id/likes', celebrate({
+  params: Joi.object()
+    .keys({
+      id: Joi.string()
+        .required()
+        .length(24),
+    }),
+}), removeLike);
 
 module.exports = router;
