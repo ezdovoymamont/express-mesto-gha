e358253b888d2735af683bf124932f3fa1f1bd89
Mamont <>
2022-10-14 13:13:31 +0300

    d
    d

--- backend/app.js
+++ backend/app.js
@@ -43,6 +43,7 @@ app.use((req, res, next) => {
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Headers', requestHeaders);
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Origin', "*");
     return res.end();
   }
 
