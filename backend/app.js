ec98b9ed6b263d35ae18a9fd7f55733f98a01aba
Mamont <>
2022-10-14 13:15:51 +0300

    d

--- backend/app.js
+++ backend/app.js
@@ -43,20 +43,18 @@ app.use((req, res, next) => {
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Headers', requestHeaders);
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-
-    console.log('CORS2');
-    const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-    console.log(origin);
-    if (allowedCors.includes(origin)) {
-      res.header('Access-Control-Allow-Origin', origin);
-    }
+    res.header('Access-Control-Allow-Origin', "*");
     return res.end();
   }
 
 
+  console.log('CORS2');
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  console.log(origin);
 
-
-
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
 
   next();
 });
